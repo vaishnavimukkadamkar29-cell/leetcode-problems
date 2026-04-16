f4731b2e3c9816cfb5a99b0ce4e1404bde8f83ed
vaishnavimukkadamkar29-cell <vaishnavimukkadamkar29@gmail.com>
2026-04-16 11:30:46 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (23.49%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -3,24 +3,19 @@ class Solution {
         int n=nums.length;
         int lo=0;
         int hi=n-1;
-        while(lo<=hi)
+        while(lo<hi)
         {
             int mid=(lo+hi)/2;
-            boolean leftok=(mid==0||nums[mid]>=nums[mid-1]);
-            boolean rightok=(mid==n-1||nums[mid]>=nums[mid+1]);
-            if(leftok&&rightok)
-            {
-                return mid;
-            }
             if(nums[mid]<nums[mid+1])
             {
                 lo=mid+1;
             }
-            else{
-                hi=mid-1;
+            else {
+                hi=mid;
             }
-        }
-        return -1;
         
+        }
+        return lo;
     }
 }
+    
