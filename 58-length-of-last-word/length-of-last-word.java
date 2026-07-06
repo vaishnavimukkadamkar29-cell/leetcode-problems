e640cbd29d44fb433df344fd96927e6e5192af5d
vaishnavimukkadamkar29-cell <vaishnavimukkadamkar29@gmail.com>
2026-07-06 19:11:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (17.32%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,11 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr = s.split(" +");
-        String last=arr[arr.length-1];
-        int ans=last.length();
-        return ans;
-
-
+        s=s.trim();
+        int lastSpaceindex=s.lastIndexOf(' ');
+        String last;
+        if(lastSpaceindex==-1)
+        {
+            last=s;
+        }
+        else{
+            last=s.substring(lastSpaceindex+1);
+        }
+return last.length();
         
     }
 }
