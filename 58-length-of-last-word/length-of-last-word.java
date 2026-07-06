ac6a220a67ff6b68b9feadfb027d91b5c382337b
vaishnavimukkadamkar29-cell <vaishnavimukkadamkar29@gmail.com>
2026-07-06 19:02:00 +0530

    Time: 2 ms (11.32%) | Memory: 43.3 MB (6.58%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,5 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int n=s.length();
         String[] arr = s.split(" +");
         String last=arr[arr.length-1];
         int ans=last.length();
