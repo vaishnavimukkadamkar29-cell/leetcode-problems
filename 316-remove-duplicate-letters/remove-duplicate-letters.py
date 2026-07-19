9680c1f5acfd852f6e475cef2e06bac14838abe3
vaishnavimukkadamkar29-cell <vaishnavimukkadamkar29@gmail.com>
2026-07-19 23:01:46 +0530

    Time: 6 ms (25.86%) | Memory: 12.4 MB (79.83%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ans=[]
+        seen=set()
+        last_occurence={char:i for i,char in enumerate(s)}
+        for i ,letter in enumerate(s):
+            if letter in seen:
+                continue
+            while ans and ans[-1]>letter and last_occurence[ans[-1]]>i:
+                removed_letter=ans.pop()
+                seen.remove(removed_letter)
+            ans.append(letter)
+            seen.add(letter)
+        return "".join(ans)
+        
