494e95112d58cb8478803a68e6ebc2462b1ece20
vaishnavimukkadamkar29-cell <vaishnavimukkadamkar29@gmail.com>
2026-07-21 21:01:11 +0530

    Time: 8 ms (96.44%) | Memory: 47.1 MB (80.51%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,20 +1,21 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
         int n = names.length;
-        Integer[] position=new Integer[n];
+        int[][] paired=new int[n][2];
         for(int i =0;i<n;i++)
         {
-            position[i]=i;
+            paired[i][0]=heights[i];
+            paired[i][1]=i;
         }
+        Arrays.sort(paired,(a,b)->Integer.compare(b[0],a[0]));
         String[] ans=new String[n];
-        Arrays.sort(position,(a,b)->Integer.compare(heights[b],heights[a]));
         for(int i =0;i<n;i++)
         {
-            int originalpos=position[i];
-            ans[i]=names[originalpos];
-        }
-        return ans;
+            int original=paired[i][1];
+            ans[i]=names[original];
 
-        
+        }
+       
+        return ans;
     }
 }
