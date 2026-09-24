edd30d3a622f105ccaaab2f38ad88f777daeaafa
vaishnavimukkadamkar29-cell <vaishnavimukkadamkar29@gmail.com>
2026-09-24 21:08:22 +0530

    Time: 10 ms (25.64%) | Memory: 42.1 MB (52.43%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,25 +1,14 @@
 class Solution {
     public int fib(int n) {
-        // 1. Create a memoization array to store already calculated results
-        // n + 1 size handles indices from 0 up to n
-        int[] memo = new int[n + 1]; 
-        
-        return helper(n, memo);
-    }
-    
-    private int helper(int n, int[] memo) {
-        // Base Cases
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        
-        // 2. If we have already calculated fib(n), return it from memory!
-        if (memo[n] != 0) {
-            return memo[n];
+        if(n==0)
+        {
+            return 0;
         }
+        if(n==1)
+        {
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
         
-        // 3. Otherwise, calculate it once and store it in our memo array
-        memo[n] = helper(n - 1, memo) + helper(n - 2, memo);
-        
-        return memo[n];
     }
 }
