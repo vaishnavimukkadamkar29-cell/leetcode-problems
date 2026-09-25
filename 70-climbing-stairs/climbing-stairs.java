385170665ad97de7e46e209b58eddb4d22ffb696
vaishnavimukkadamkar29-cell <vaishnavimukkadamkar29@gmail.com>
2026-09-25 10:33:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (54.62%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    int[] dp;
+    public int climbStairs(int n) {
+        dp=new int[n+1];
+       return solve(n);
+    }
+    public int solve(int n)
+    {
+        if(n==1)
+        {
+            return 1;
+        }
+        if(n==2)
+        {
+            return 2;
+        }
+        if(dp[n]!=0)
+        {
+            return dp[n];
+        }
+        dp[n]=solve(n-1)+solve(n-2);
+        return dp[n];
+    }
+}
