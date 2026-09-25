977e24fcb60816ad1e5048bab46d697fe5af71a1
vaishnavimukkadamkar29-cell <vaishnavimukkadamkar29@gmail.com>
2026-09-25 14:07:34 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (21.46%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,6 +1,30 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
-        
+       long N=n;
+       if(N<0)
+       {
+        x=1/x;
+        N=-N;
+       }
+       return power(x,N);
+
+    }
+    public double power(double x,long N)
+    {
+        if(N==0)
+        {
+            return 1;
+        }
+        if(N==1)
+        {
+            return x;
+        }
+        double half=power(x,N/2);
+
+        if(N%2==0)
+        {
+            return half*half;
+        }
+        return half*half*x;
     }
 }
